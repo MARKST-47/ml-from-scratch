65be1f283e808bb41141453a2ba2d3d7d8181a3c
Mark <markstephenthomas10@gmail.com>
2026-08-10 17:42:03 +0200

    vae class

--- vae.py
+++ vae.py
@@ -15,6 +15,38 @@ train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 latent_dim = 64
 
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim=784, hidden_dim=256, latent_dim=latent_dim):
         super().__init__()
+        # Encoder
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc_mu = nn.Linear(hidden_dim, latent_dim)
+        self.fc_logvar = nn.Linear(hidden_dim, latent_dim) # Since log(var) allows -inf to 0 vals and can be reversed with exp to get var
+        # Decoder
+        self.fc2 = nn.Linear(latent_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, input_dim)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        
+    def encode(self, x):
+        h = self.relu(self.fc1(x))
+        mu = self.fc_mu(h)
+        logvar = self.fc_logvar(h)
+        return mu, logvar
+        
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        z = mu + eps * std
+        return z
+        
+    def decode(self, z):
+        h = self.relu(self.fc2(z))
+        out = self.sigmoid(self.fc3(h))
+        return out
+             
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        x_reconstructed = self.decode(z)
+        return x_reconstructed, mu, logvar    
         
